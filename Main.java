f4043c62dc525345b69784fd7335f6662e85fe0c
bida <abidanuraini0@gmail.com>
2026-03-13 13:38:30 -1200

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Weapon pedangSakti = new Weapon();
+        pedangSakti.name = "Pedang Excalibur";
+        pedangSakti.damage = 150;
+        pedangSakti.showWeapon();
+    }
+}
---
+++ Weapon.java
@@ -0,0 +1,10 @@
+public class Weapon {
+
+    String name;
+    int damage;
+
+    public void showWeapon() {
+        System.out.println("Nama Senjata : " + name);
+        System.out.println("Damage       : " + damage);
+    }
+}
